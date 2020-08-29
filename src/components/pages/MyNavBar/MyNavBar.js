9087870dac92aada0369fe8c65dab9cfbfec8a46
Geno McNew <genemcnew3@gmail.com>
2020-08-29 11:22:47 -0500

    import reactstrap navbar

--- src/components/pages/MyNavBar/MyNavBar.js
+++ src/components/pages/MyNavBar/MyNavBar.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
 
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
 class MyNavBar extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
+  toggle = () => {
+    const { isOpen } = this.state;
+    this.setState({ isOpen: !isOpen });
+  }
+
   loadLogOut = () => {
     const { authed } = this.props;
     if (authed) {
@@ -24,13 +47,42 @@ class MyNavBar extends React.Component {
   }
 
   render() {
+    const { isOpen } = this.state;
     return (
-      <div>
-        <nav className="navbar navbar-light bg-light">
-          <div className="navbar-brand">Birb Watcher</div>
-          {this.loadLogOut()}
-        </nav>
-      </div>
+        <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
     );
   }
 }
