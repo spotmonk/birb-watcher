73211136c55775190dafe9c2445000235e9dd961
Geno McNew <genemcnew3@gmail.com>
2020-08-29 10:44:33 -0500

    routing to components setup

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.scss';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 import fbConnection from '../helpers/data/connection';
 
@@ -10,9 +16,22 @@ import Home from '../components/pages/Home/Home';
 import EditBirb from '../components/pages/EditBirb/EditBirb';
 import NewBirb from '../components/pages/NewBirb/NewBirb';
 import SingleBirb from '../components/pages/SingleBirb/SingleBirb';
+import Auth from '../components/pages/Auth/Auth';
 
 fbConnection();
 
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 class App extends React.Component {
   state = {
     authed: false,
@@ -33,12 +52,22 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <MyNavBar authed={authed} />
-        <Home />
-        <EditBirb />
-        <NewBirb />
-        <SingleBirb />
-      </div>
+       <BrowserRouter>
+        <React.Fragment>
+          <MyNavBar authed={authed}/>
+          <div className="container">
+            <Switch>
+              <PrivateRoute path="/home" component={Home} authed={authed} />
+              <PrivateRoute path="/new" component={NewBirb} authed={authed} />
+              <PrivateRoute path="/edit/:birbId" component={EditBirb} authed={authed} />
+              <PrivateRoute path="/birbs/:birdId" component={SingleBirb} authed={authed} />
+              <PublicRoute path="/auth" component={Auth} authed={authed} />
+              <Redirect from="*" to="/home" />
+            </Switch>
+          </div>
+        </React.Fragment>
+       </BrowserRouter>
+       </div>
     );
   }
 }
