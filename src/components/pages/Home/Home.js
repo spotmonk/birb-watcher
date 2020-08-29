32574f8cdcfd6b0c5a2f51840905da81bbeff01f
Geno McNew <genemcnew3@gmail.com>
2020-08-29 10:56:51 -0500

    birb routing working

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 class Home extends React.Component {
+  editBirbEvent = (e) => {
+    e.preventDefault();
+    const birbId = 'birb1000';
+    this.props.history.push(`/edit/${birbId}`);
+  }
+
   render() {
     return (
-      <h2>Home Component</h2>
+      <button className="btn btn-dark" onClick={this.editBirbEvent} >Edit a Birb</button>
     );
   }
 }
